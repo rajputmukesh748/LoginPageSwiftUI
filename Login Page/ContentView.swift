2e8c5e5ba6283e7da2050ab8bc4b09675277f922
Mukesh Kumar <rajputmukesh748@gmail.com>
2024-08-26 18:52:44 +0530

    Initial Commit

---
+++ Login Page/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Login Page
+//
+//  Created by Mukesh Rajput on 26/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Login Page/Login_PageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_PageApp.swift
+//  Login Page
+//
+//  Created by Mukesh Rajput on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Login_PageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
